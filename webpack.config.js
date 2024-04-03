259aa6a93b0cf0aa04f7416ff003fdf3cf758444
github8223 <165763123+github8223@users.noreply.github.com>
2024-04-03 01:39:52 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,12 @@
-webpack.config.js
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    fallback: {
+      "buffer": require.resolve("buffer/"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "zlib": require.resolve("browserify-zlib")
+    }
+  }
+};
